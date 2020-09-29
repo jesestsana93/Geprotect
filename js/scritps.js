c2bc024be92822148e726c57f3a11f9c2693e316
chuyudemy <chuyunam93@gmail.com>
2020-09-29 02:37:11 -0500

    falta ajustar imagen de fondo

---
+++ js/scritps.js
@@ -0,0 +1,29 @@
+$(function () {
+    /*-----------------------------------------------------------
+    1. FUNCIONES PARA EL MENU PRINCIPAL
+    -------------------------------------------------------------*/
+    /*  Inserta y quita la clase ".icono-cerrar" al boton del menú*/
+    $('#menu-navegacion .navbar-toggler').click(function () {
+        $('.boton-menu').toggleClass('icono-cerrar');
+    });
+
+    /*Al hacer click en un enlace del menú principal */
+    $('#menu-navegacion .navbar-nav a').click(function () {
+
+        /* 1) Quita la clase ".icono-cerrar" */
+        $('.boton-menu').removeClass('icono-cerrar');
+
+        /*2) Contrae el menu */
+        $('#menu-navegacion .navbar-collapse').collapse('hide');
+    });
+
+    /*-----------------------------------------------------
+    2. INICIANDO "page-scroll-to-id"
+     -------------------------------------------------------------*/
+    $('#menu-principal a').mPageScroll2id({
+        offset: 50,
+        highlightClass: 'active'
+    });
+   
+
+});
